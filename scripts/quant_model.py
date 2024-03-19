9cae74098bf042a19ae1aaf55f6fb3fbadc80b3c
Zigarov <jluk.376@gmail.com>
2024-03-19 15:55:33 +0100

    quant_model

--- scripts/quant_model.py
+++ scripts/quant_model.py
@@ -1,24 +1,30 @@
 """
-This code is extracted from ./guided-diffusion/scripts/quant_image_sample.py
-It quantized the model provided
+Generate a large batch of image samples from a model and save them as a large
+numpy array. This can be used to produce samples for FID evaluation.
 """
 
 import argparse
 import os
 
 import numpy as np
-import torch.nn as nn
-import torch
+import matplotlib.pyplot as plt
+import torch as th
 import torch.distributed as dist
+import torchvision as tv
+from scipy import ndimage, signal
+from pooling import MedianPool2d
 
 from guided_diffusion import dist_util, logger
+from guided_diffusion.image_datasets import load_data
+
+# from guided_diffusion import dist_util, logger
 from guided_diffusion.script_util import (
-    NUM_CLASSES,
     model_and_diffusion_defaults,
     create_model_and_diffusion,
     add_dict_to_argparser,
     args_to_dict,
 )
+
 from QDrop.quant import (
     block_reconstruction,
     layer_reconstruction,
@@ -28,8 +34,18 @@ from QDrop.quant import (
     set_weight_quantize_params,
     set_act_quantize_params,
 )
-from QDrop.data.imagenet import build_imagenet_data
 
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+# SNR (var): 1 (0.9) 5 (0.6) 10 (0.36) 15 (0.22) 20 (0.13) 25 (0.08) 30 (0.05) 100 (0.0)
+SNR_DICT = {100: 0.0,
+            30: 0.05,
+            25: 0.08,
+            20: 0.13,
+            15: 0.22,
+            10: 0.36,
+            5: 0.6,
+            1: 0.9}
 
 def random_calib_data_generator(shape, num_samples, device,class_cond=True):
     logger.log('Calibration Data Generation...')
@@ -149,21 +165,17 @@ def quant_model(args, cnn):
         return qnn
 
 
+
 def main():
     args = create_argparser().parse_args()
-
-    dist_util.setup_dist()
+    # dist_util.setup_dist()
     logger.configure()
-
-    logger.log("creating model and diffusion...")
+    print("creating model and diffusion...")
     model, diffusion = create_model_and_diffusion(
         **args_to_dict(args, model_and_diffusion_defaults().keys())
     )
-    model.load_state_dict(
-        dist_util.load_state_dict(args.model_path, map_location="cpu")
-    )
-    model.to(dist_util.dev())
-    model.dtype = torch.float32
+    model.load_state_dict(th.load(args.model_path))
+    model.to("cuda")
     # if args.use_fp16:
     #     model.convert_to_fp16()
     model.eval()
@@ -171,19 +183,139 @@ def main():
     # quantize model
     model = quant_model(args, model)
     
-    logger.log("Model Quantized!")
+
 
 def create_argparser():
     defaults = dict(
+        data_dir="",
+        dataset_mode="",
         clip_denoised=True,
         num_samples=10000,
-        batch_size=16,
+        batch_size=1,
         use_ddim=False,
         model_path="",
+        results_path="",
+        is_train=False,
+        num_classes=35,
+        s=1.0,
+        snr=100,
+        pool="med",
+        add_noise=False,
+        noise_to="semantics",
+        unet_model="unet" #"unet", "spadeboth", "enconly"
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
     add_dict_to_argparser(parser, defaults)
+
+    parser.add_argument(
+        "--seed", default=1005, type=int, help="random seed for results reproduction"
+    )
+
+    # quantization parameters
+    parser.add_argument(
+        "--n_bits_w", default=4, type=int, help="bitwidth for weight quantization"
+    )
+    parser.add_argument(
+        "--channel_wise",
+        action="store_true",
+        help="apply channel_wise quantization for weights",
+    )
+    parser.add_argument(
+        "--n_bits_a", default=4, type=int, help="bitwidth for activation quantization"
+    )
+    parser.add_argument(
+        "--act_quant", action="store_true", help="apply activation quantization"
+    )
+    parser.add_argument("--disable_8bit_head_stem", action="store_true")
+
+    # weight calibration parameters
+    parser.add_argument(
+        "--calib_num_samples",
+        default=1024,
+        type=int,
+        help="size of the calibration dataset",
+    )
+    parser.add_argument(
+        "--iters_w", default=20000, type=int, help="number of iteration for adaround"
+    )
+    parser.add_argument(
+        "--weight",
+        default=0.01,
+        type=float,
+        help="weight of rounding cost vs the reconstruction loss.",
+    )
+    parser.add_argument(
+        "--keep_cpu", action="store_true", help="keep the calibration data on cpu"
+    )
+
+    parser.add_argument(
+        "--wwq",
+        action="store_true",
+        help="weight_quant for input in weight reconstruction",
+    )
+    parser.add_argument(
+        "--waq",
+        action="store_true",
+        help="act_quant for input in weight reconstruction",
+    )
+
+    parser.add_argument(
+        "--b_start",
+        default=20,
+        type=int,
+        help="temperature at the beginning of calibration",
+    )
+    parser.add_argument(
+        "--b_end", default=2, type=int, help="temperature at the end of calibration"
+    )
+    parser.add_argument(
+        "--warmup",
+        default=0.2,
+        type=float,
+        help="in the warmup period no regularization is applied",
+    )
+
+    # activation calibration parameters
+    parser.add_argument("--lr", default=4e-5, type=float, help="learning rate for LSQ")
+
+    parser.add_argument(
+        "--awq",
+        action="store_true",
+        help="weight_quant for input in activation reconstruction",
+    )
+    parser.add_argument(
+        "--aaq",
+        action="store_true",
+        help="act_quant for input in activation reconstruction",
+    )
+
+    parser.add_argument(
+        "--init_wmode",
+        default="mse",
+        type=str,
+        choices=["minmax", "mse", "minmax_scale"],
+        help="init opt mode for weight",
+    )
+    parser.add_argument(
+        "--init_amode",
+        default="mse",
+        type=str,
+        choices=["minmax", "mse", "minmax_scale"],
+        help="init opt mode for activation",
+    )
+    # order parameters
+    parser.add_argument(
+        "--order",
+        default="before",
+        type=str,
+        choices=["before", "after", "together"],
+        help="order about activation compare to weight",
+    )
+    parser.add_argument("--prob", default=1.0, type=float)
+    parser.add_argument("--input_prob", default=1.0, type=float)
+    parser.add_argument("--use_adaround", action="store_true")
+
     return parser
 
 
